0d74280d76c807a045837ec9e9834eadd78467f8
priya dharshini <priya dharshini@priyadharshini>
2021-05-23 09:12:00 +0530

    first commit

---
+++ adm017-springboot/src/main/java/com/cts/Adm017SpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.cts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Adm017SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Adm017SpringbootApplication.class, args);
+	}
+
+}
