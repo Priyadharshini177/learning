81a09106652e10d35daca4e341ded2fbb29f81f9
priya dharshini <priya dharshini@priyadharshini>
2021-05-23 12:38:01 +0530

    second

---
+++ adm017-springcore/src/main/java/com/cts/spring/Employee.java
@@ -0,0 +1,34 @@
+package com.cts.spring;
+
+public class Employee {
+	private int EmployeeId;
+	private String EmployeeName;
+	private double salary;
+	private int age;
+	public int getEmployeeId() {
+		return EmployeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		EmployeeId = employeeId;
+	}
+	public String getEmployeeName() {
+		return EmployeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		EmployeeName = employeeName;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+
+}
---
+++ adm017-springcore/src/main/webapp/RegistrationPage.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<form action="regserv" method="post">
+Enter User Name: <input type="text" name="username"><br>
+EnterbPassword:<input type="password" name="password"><br>
+Enter Email Id: <input type="text" name="email"><br>
+
+<input type="submit" name="submit" value="submit">
+
+</form>
+
+</body>
+</html>
